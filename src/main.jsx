55a146d629369d98d305551c03c5f6546dab508e
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-03 14:40:49 +0300

    wrapped the ReactRouterProvider in the main.jsx with cartContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from '../routes.jsx'
-
+import { CartContextProvider } from '../store/cartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}>
-      
-    </RouterProvider>
-    
+    <CartContextProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </CartContextProvider>
   </StrictMode>,
 )
