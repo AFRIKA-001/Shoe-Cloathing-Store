056434477c06d258283ef707fa17889a6d944750
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-03 12:14:06 +0300

    added a footer in the root layout

--- RootLayout.jsx
+++ RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./src/components/NavBar.jsx"
+import Footer from "./src/components/footer.jsx";
 
 function RootLayout() {
   return (
     <div>
      <NavBar/>
       <Outlet/>
+      <Footer/>
     </div>
   )
 }
