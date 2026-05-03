219d9249d13a21f5bd6e513a5af5b3c3180db9a0
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-03 11:36:59 +0300

    added a scale of 105 to the product grids on hover

--- customProductHook.jsx
+++ customProductHook.jsx
@@ -2,9 +2,9 @@
 function CustomProductHook({product}) {
   return (
     <div>
-      <img src={product.image} alt={product.name} className="h-70 w-70 object-cover" />
+      <img src={product.image} alt={product.name} className="h-70 w-70 object-cover hover:scale-105" />
       <h2 className="text-black font-serif">{product.name}
-        <span className="font-thin text-gray-600   "> {product.size}</span>
+        <span className="font-thin text-gray-600 "> {product.size}</span>
         </h2>   
       <p className="font-bold font-serif text-xl ">KES {product.price}</p>
     </div>
