d9800da602b386b7ae07c827ae8ec857e6d402d8
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-03 12:21:34 +0300

    added some more sneakers

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ const DUMMY_PRODUCTS =[
   {id:'p3',name:'modern-athletic runner',price:2500,size:43 ,image:'https://images.unsplash.com/photo-1549298916-b41d501d3772?auto=format&fit=crop&q=80&w=1000'},
   {id:'p4',name:'Blue suede Urban shoe',price:2500,size:41 ,image:'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?auto=format&fit=crop&q=80&w=1000'},
   {id:'p5',name:'Leather Puma shoe',price:2500,size:38 ,image:'https://images.unsplash.com/photo-1608231387042-66d1773070a5?auto=format&fit=crop&q=80&w=1000'},
+  {id:'p6',name:'Leather Puma shoe',price:2500,size:38 ,image:'https://images.unsplash.com/photo-1560769629-975ec94e6a86?auto=format&fit=crop&q=80&w=1000'},
+  {id:'p7',name:'Leather Nike sneaker',price:2500,size:38 ,image:'https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?auto=format&fit=crop&q=80&w=1000'},
+  {id:'p',name:'Leather Nike shoe',price:2500,size:38 ,image:'https://images.unsplash.com/photo-1539185441755-769473a23570?auto=format&fit=crop&q=80&w=1000'},
 
 ]
 
