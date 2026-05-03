1af17ad7995220524f607262179bd67437592075
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-03 14:39:48 +0300

    implemented the cart context

--- customProductHook.jsx
+++ customProductHook.jsx
@@ -1,22 +1,24 @@
-
-const formatter = new Intl.NumberFormat('en-US',{
-    style:'currency',
-    currency:"ksh",
+import { Link } from 'react-router-dom'
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: "ksh",
 })
 
 
 
+function CustomProductHook({ product }) {
+    return (
+        <div>
+            <Link to={`/:id${product.id}`}>
+                <img src={product.image} alt={product.name} className="h-70 w-85 lg:w-70 object-cover hover:scale-105" />
+                <h2 className="text-black font-serif">{product.name}
+                    <span className="font-thin text-gray-600 "> {product.size}</span>
+                </h2>
+                <p className="font-bold font-serif text-xl "> {formatter.format(product.price)}</p>
+            </Link>
 
-function CustomProductHook({product}) {
-  return (
-    <div>
-      <img src={product.image} alt={product.name} className="h-70 w-85 lg:w-70 object-cover hover:scale-105" />
-      <h2 className="text-black font-serif">{product.name}
-        <span className="font-thin text-gray-600 "> {product.size}</span>
-        </h2>   
-      <p className="font-bold font-serif text-xl "> {formatter.format(product.price)}</p>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default CustomProductHook;
