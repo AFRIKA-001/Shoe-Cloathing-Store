587f91bcba11ee5f950fdd09b29f8407604846b6
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-03 12:12:09 +0300

    added a foooter and implemented it in the rootLayout to be displayed in every page

---
+++ src/components/footer.jsx
@@ -0,0 +1,21 @@
+
+
+function Footer() {
+  return (
+    <div className="bg-black h-50 text-white ">
+      <ul className="grid grid-cols-3 lg:grid-cols-4 justify-items-center py-8 space-y-6">
+        <li>Mens</li>
+        <li>Women</li>
+        <li>Kids</li>
+        <li>New arrivals</li>
+        <li>linkedn</li>
+        <li>instagram</li>
+        <li>X</li>
+    
+      </ul>
+      <p className="flex justify-center text-gray-300">&copy; all rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer;
