4533cd605baf6ab5a7369749c5c20c723e5dc634
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-05 03:37:04 +0300

    added social media links to the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,26 @@
-
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaInstagram, FaTiktok } from "react-icons/fa"
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className="bg-black h-50 text-white ">
-      <ul className="grid grid-cols-3 lg:grid-cols-4 justify-items-center py-8 space-y-6 ">
-        <li>Mens</li>
-        <li>Women</li>
-        <li>Kids</li>
-        <li>New arrivals</li>
-        <li>linkedn</li>
-        <li>instagram</li>
-        <li>X</li>
-    
+    <div className="bg-black h-70 text-white bottom absolute w-full ">
+      <ul className="grid grid-cols-4 lg:grid-cols-4 justify-items-center py-8 space-y-6  ">
+        <li><Link to ='/men' className="hover:text-red-500">Men</Link> </li>
+        <li> <Link to ='/women'  className="hover:text-red-500">Women</Link></li>
+          <li> <Link to ='/kids'  className="hover:text-red-500">kids</Link></li>
+            <li> <Link to ='/newarrivals'  className="hover:text-red-500">New Arrivals</Link></li>
       </ul>
-      <p className="flex justify-center text-gray-300">&copy; all rights reserved.</p>
+      <h1 className=" pl-12 lg:pl-25 pb-4 underline">Follow us </h1>
+      <div className="grid grid-cols-4 lg:grid-cols-4 justify-items-center list-none space-y-6">
+          
+          <li> <a href="https://www.linkedin.com/in/gabriel-wafula/" target="_blank" rel="noopener noreferrer"><FaLinkedin  className="text-blue-700"/> </a></li>
+        <li><a href="https://www.instagram.com/developer_gabriel/" target="_blank" rel="noopener noreferrer"><FaInstagram className="text-pink-600"/></a></li>
+        <li><a href="https://www.tiktok.com/@afrikan_hero1" target="_blank" rel="noopener noreferrer" ><FaTiktok className="text-pink-600"/></a></li>
+        <li><a href="" target="_blank" rel="noopener noreferrer"><FaXTwitter /></a></li>
+    
+      </div>
+      <p className="flex justify-center text-gray-300 pt-6">&copy; jaha fashion and design.ac.ke</p>
     </div>
   )
 }
