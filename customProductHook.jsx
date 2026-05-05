33afa35b9d746e419511cc237f464ddfb1aaa70a
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-05 13:36:03 +0300

    made some styling changes to the images and prices

--- customProductHook.jsx
+++ customProductHook.jsx
@@ -10,11 +10,11 @@ function CustomProductHook({ product }) {
     return (
         <div>
             <Link to={`/:id${product.id}`}>
-                <img src={product.image} alt={product.name} className="h-70 w-85 lg:w-70 object-cover hover:scale-105" />
-                <h2 className="text-black font-medium mt-4 ">{product.name}
-                    <span className="font-normal  text-gray-500 "> size: {product.size}</span>
+                <img src={product.image} alt={product.name} className="h-48 w-full lg:h-70 object-cover hover:scale-105 hover:shadow-xl transition-shadow duration-300 aspect-video" />
+                <h2 className="text-gray-800 line-clamp-2 font-sans mt-4 ">{product.name}
+                    <span className="font-sans font-light  text-gray-600 "> size: {product.size}</span>
                 </h2>
-                <p className=" tracking-tighter font-mono font-bold lg:text-lg "> {formatter.format(product.price)}</p>
+                <p className=" tracking-tight text-xs font-sans font-bold lg:text-lg text-gray-800 "> {formatter.format(product.price)}</p>
             </Link>
 
         </div>
