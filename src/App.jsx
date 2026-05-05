8ba9a8a35cd8f6c5c4f027000fc61ccde0faa6ab
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-05 13:49:00 +0300

    added a span to the treat youself text and wrapped it to the Jaha word an give it a white color property

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ if(isLoading){
     
     <div className=" flex flex-col bg-[url(https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&q=60&w=900)] bg-cover bg-center bg-no-repeat h-screen  w-full">
     <h1 className="text-4xl  lg:text-7xl font-bold tracking-tight text-white leading-snug ">Have your Next generation aspects of fashion</h1>
-    <p className="text-2xl text-gray-400">treat yourself with sneakers from Jaha</p>
+    <p className="text-2xl pl-2 text-gray-400">Treat yourself with sneakers & fashion from <span className="text-white">Jaha</span></p>
     <button className="flex border-white font-bold bg-white  justify-start mt-40 lg:mt-20 max-w-60 border ml-6 mx-auto p-2 px-8 rounded-xl  hover:bg-gray-400 transition-all transform hover:scale-105 active:scale-95">
      <Link to='/women'>shop now</Link> 
       </button>
