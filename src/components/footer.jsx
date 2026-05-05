0fc3fb11110e41f1ddc500ce8cfd5aeba720b63d
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-05 14:20:51 +0300

    animated the social media links with framer motion

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { FaLinkedin, FaInstagram, FaTiktok } from "react-icons/fa"
 import { FaXTwitter } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 function Footer() {
   return (
@@ -12,14 +13,14 @@ function Footer() {
             <li> <Link to ='/newarrivals'  className="hover:text-red-500">New Arrivals</Link></li>
       </ul>
       <h1 className=" pl-12 lg:pl-25 pb-4 underline">Follow us </h1>
-      <div className="grid grid-cols-4 lg:grid-cols-4 justify-items-center list-none space-y-6">
+      <motion.div animate={{y:[10,100,10]}} className="grid grid-cols-4 lg:grid-cols-4 justify-items-center list-none space-y-6">
           
           <li> <a href="https://www.linkedin.com/in/gabriel-wafula/" target="_blank" rel="noopener noreferrer"><FaLinkedin  className="text-blue-700"/> </a></li>
         <li><a href="https://www.instagram.com/developer_gabriel/" target="_blank" rel="noopener noreferrer"><FaInstagram className="text-pink-600"/></a></li>
         <li><a href="https://www.tiktok.com/@afrikan_hero1" target="_blank" rel="noopener noreferrer" ><FaTiktok className="text-pink-600"/></a></li>
         <li><a href="" target="_blank" rel="noopener noreferrer"><FaXTwitter /></a></li>
     
-      </div>
+      </motion.div>
       <p className="flex justify-center text-gray-300 pt-6">&copy; jaha fashion and design.ac.ke</p>
     </div>
   )
