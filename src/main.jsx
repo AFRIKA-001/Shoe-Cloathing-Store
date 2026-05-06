6021739c2782d9e6687873254bc4938c77356197
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-07 00:20:27 +0300

    wrapped the authContext provider in the main.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from '../routes.jsx'
 import { CartContextProvider } from '../store/cartContext.jsx'
+import { UserAuthContextProvider } from '../store/authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CartContextProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </CartContextProvider>
+    <UserAuthContextProvider>
+      <CartContextProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </CartContextProvider>
+    </UserAuthContextProvider>
+
   </StrictMode>,
 )
