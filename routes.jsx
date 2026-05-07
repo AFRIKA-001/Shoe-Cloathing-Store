6e857aed601c7f1fe5739b8d1cd02ed21b9a372f
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-07 11:32:18 +0300

    protected men,women,kids and cart routes using the protectedRoutes component

--- routes.jsx
+++ routes.jsx
@@ -9,6 +9,7 @@ import DetailsPage from "./src/components/DetailsPage.jsx";
 import Cart from "./src/components/Cart.jsx";
 import SignIn from "./src/components/Logins/SignIn.jsx";
 import SignUp from "./src/components/Logins/SignUp.jsx";
+import ProtectedRoute from "./src/components/Protected/ProtectedRoute.jsx";
 
 
 const router = createBrowserRouter([
@@ -17,11 +18,11 @@ const router = createBrowserRouter([
         element:<RootLayout />,
         children:[
         {index:true,element:<App/>},
-        {path:'/cart', element:<Cart/>},
-        {path:'/men',element:<Mens/>},
-        {path:'/women',element:<Women/>},
-        {path:'/kids',element:<Kids/>},
-        {path:'/newarrivals',element:<New/>},
+        {path:'/cart', element:<ProtectedRoute><Cart/></ProtectedRoute>},
+        {path:'/men',element:<ProtectedRoute><Mens/></ProtectedRoute>},
+        {path:'/women',element:<ProtectedRoute><Women/></ProtectedRoute>},
+        {path:'/kids',element:<ProtectedRoute><Kids/></ProtectedRoute>},
+        {path:'/newarrivals',element:<ProtectedRoute><New/></ProtectedRoute>},
         {path:'/signin',element:<SignIn/>},
         {path:'/signup',element:<SignUp/>},
     
