a2864a6fb142caf4a2cc9a71f9f1f4556ff27a32
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-08 23:37:11 +0300

    added some stylings to the custominput field

--- src/components/Logins/CustomInput.jsx
+++ src/components/Logins/CustomInput.jsx
@@ -2,7 +2,7 @@ const Input = ({ label,id,name,type,onChange,placeholder }) => {
   return (
     <div>
       <label >{label}</label>
-      <input name={name} type={type} id={id}  onChange={onChange} placeholder={placeholder} required className="border rounded w-full pl-1 " />
+      <input name={name} type={type} id={id}  onChange={onChange} placeholder={placeholder} required className="border rounded w-full pl-1 bg-white tracking-wide outline-1  py-1 " />
     </div>
   )
 }
