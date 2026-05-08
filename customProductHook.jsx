f1a3eed84a7406d8452e8c0ae9cccb151defb4e4
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-08 23:20:51 +0300

    managed the add to cart button to display a green text that say added to cart and also to togle back when clicked again and decrement the number of items

--- customProductHook.jsx
+++ customProductHook.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext,useState } from 'react';
 import { Link } from 'react-router-dom'
 import CartContext from './store/cartContext';
 
@@ -11,9 +11,15 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 function CustomProductHook({ product,tableName }) {
     const cartContext = useContext(CartContext)
+    const [addedToCart, setAddedToCart] = useState(false);
 
     function addToCart(){
+        if(!addedToCart){
         cartContext.AddItems(product)
+        }else{
+        cartContext.RemoveItems(product.id)
+        }
+        setAddedToCart(!addedToCart);
     }
     return (
         <div className='flex flex-col items-center gap-4'>
@@ -24,7 +30,10 @@ function CustomProductHook({ product,tableName }) {
                 </h2>
                 <p className=" tracking-tight text-xs font-sans font-bold lg:text-lg text-gray-800 "> {formatter.format(product.price)}</p>
             </Link>
-            <button onClick={addToCart} className='border rounded-full w-full p-1 hover:scale-105 bg-black hover:bg-slate-700 text-white transition-all duration-400 active:scale-95'>Add to Cart</button>
+            <button onClick={addToCart} className='border rounded-full w-full p-1 hover:scale-105 bg-black hover:bg-slate-700 text-white transition-all duration-400 active:scale-95'>
+                {addedToCart ? <p className='text-green-500'>Added to Cart<span className="pl-1 text-green-500">✓</span></p> : 'Add to Cart'}
+                {!addedToCart && <span className="pl-1">+</span>}
+                </button>
 
         </div>
     )
