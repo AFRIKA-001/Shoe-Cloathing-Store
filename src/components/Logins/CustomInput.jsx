7124acf8d8d934ece11f73150182c4b3aa8628b4
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-08 12:18:06 +0300

    added a padding left of 1 to the inputs in order to display the  blink pointer

--- src/components/Logins/CustomInput.jsx
+++ src/components/Logins/CustomInput.jsx
@@ -2,7 +2,7 @@ const Input = ({ label,id,name,type,onChange}) => {
   return (
     <div>
       <label >{label}</label>
-      <input name={name} type={type} id={id}  onChange={onChange} required className="border rounded w-full " />
+      <input name={name} type={type} id={id}  onChange={onChange} required className="border rounded w-full pl-1 " />
     </div>
   )
 }
