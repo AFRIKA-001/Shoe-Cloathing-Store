89495e2df4bbc63e3d39d6aef6bbf643fc98b38a
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-25 12:03:39 +0300

    added lazy laoding to the whole component

--- src/components/Kids.jsx
+++ src/components/Kids.jsx
@@ -1,38 +1,54 @@
-import { supabase } from "../../store/supabaseClient"
-import CustomProductHook from '../../customProductHook'
-import  {useState , useEffect}  from "react"
-import { Loader2 } from "lucide-react";
-
+import { supabase } from "../../store/supabaseClient";
+import { useState, useEffect, lazy, Suspense } from "react";
+import { Loader, Loader2 } from "lucide-react";
+const CustomProductHook = lazy(() => import("../../customProductHook"));
 
 function Kids() {
-const [shoes,setShoes] = useState([]);
-const [isLoading, setIsLoading]=useState(false);
+  const [shoes, setShoes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     const KidsDataFetching = async () => {
-      setIsLoading(true)
+      setIsLoading(true);
 
-      const {data,error:supabaseError}= await supabase.from('kids_table').select('*');
-      if(supabaseError){
+      const { data, error: supabaseError } = await supabase
+        .from("kids_table")
+        .select("*");
+      if (supabaseError) {
         return supabaseError;
-      }else{
-        setShoes(data)
+      } else {
+        setShoes(data);
       }
-      setIsLoading(false)
-    }
+      setIsLoading(false);
+    };
     KidsDataFetching();
-  },[])
-  if(isLoading){
-    return <div className="text-red-600 flex justify-center my-60 animate-spin  "><Loader2/></div>
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="text-red-600 flex justify-center my-60 animate-spin  ">
+        <Loader2 />
+      </div>
+    );
   }
   return (
-    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
-      {shoes.map(product=><li key={product.id}>
-        <CustomProductHook product={product} tableName="kids_table"/>
-      </li>)}
-    </ul>
-  )
+    <>
+      <Suspense
+        fallback={
+          <div className="flex justify-center py-60">
+            <Loader className="animate-spin text-gray" />
+          </div>
+        }
+      >
+        <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
+          {shoes.map((product) => (
+            <li key={product.id}>
+              <CustomProductHook product={product} tableName="kids_table" />
+            </li>
+          ))}
+        </ul>
+      </Suspense>
+    </>
+  );
 }
 
-export default Kids
+export default Kids;
