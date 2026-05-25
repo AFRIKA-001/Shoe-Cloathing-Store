31f3f794c900677f7067450ab79cc46e37193314
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-25 11:00:00 +0300

    added Toaster flobally here

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,22 @@ import { RouterProvider } from 'react-router-dom'
 import router from '../routes.jsx'
 import { CartContextProvider } from '../store/cartContext.jsx'
 import { UserAuthContextProvider } from '../store/authContext.jsx'
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserAuthContextProvider>
       <CartContextProvider>
+        <Toaster position="top-left" reverseOrder={false} toastOptions={{
+    duration: 1000,
+    style: {
+      background: "#111827",
+      color: "#fff",
+      borderRadius: "14px",
+      padding: "14px 18px",
+      fontWeight: "600",
+    },
+  }}/>
         <RouterProvider router={router}></RouterProvider>
       </CartContextProvider>
     </UserAuthContextProvider>
