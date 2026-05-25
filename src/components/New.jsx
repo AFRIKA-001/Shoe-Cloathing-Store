12eec71356e5c16fde9eccfc254c2ebc10d0356b
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-25 12:04:01 +0300

    added lazy laoding to the whole component

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,37 +1,57 @@
-import { supabase } from "../../store/supabaseClient"
-import CustomProductHook from '../../customProductHook'
-import  {useState , useEffect}  from "react"
-import { Loader2 } from "lucide-react";
-
+import { supabase } from "../../store/supabaseClient";
+import { useState, useEffect, lazy, Suspense } from "react";
+import { Loader, Loader2 } from "lucide-react";
+const CustomProductHook = lazy(() => import("../../customProductHook"));
 
 function New() {
-  const [data,setData] = useState([]);
-  const [isLoading,setIsLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(()=>{
-    const newArrivalDataFetching= async() => {
-      setIsLoading(true)
-      const {data,error} = await supabase.from('newarrivals_table').select('*');
-      if(error){
-        console.error('There was an error fetching data from supabase')
-      } else{
+  useEffect(() => {
+    const newArrivalDataFetching = async () => {
+      setIsLoading(true);
+      const { data, error } = await supabase
+        .from("newarrivals_table")
+        .select("*");
+      if (error) {
+        console.error("There was an error fetching data from supabase");
+      } else {
         setData(data || []);
       }
-      setIsLoading(false)
-    }
+      setIsLoading(false);
+    };
     newArrivalDataFetching();
-  },[]);
+  }, []);
 
-  if(isLoading){
-    return <div className="text-red-600 flex justify-center my-60 animate-spin" ><Loader2/></div>
+  if (isLoading) {
+    return (
+      <div className="text-red-600 flex justify-center my-60 animate-spin">
+        <Loader2 />
+      </div>
+    );
   }
   return (
-    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-      {data.map(product => <li key={product.id}>
-        <CustomProductHook product={product} tableName="newarrivals_table" />
-      </li> )}
-    </ul>
-  )
+    <>
+      <Suspense
+        fallback={
+          <div className="flex justify-center my-60">
+            <Loader className="text-red-600 animate-spin" />
+          </div>
+        }
+      >
+        <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+          {data.map((product) => (
+            <li key={product.id}>
+              <CustomProductHook
+                product={product}
+                tableName="newarrivals_table"
+              />
+            </li>
+          ))}
+        </ul>
+      </Suspense>
+    </>
+  );
 }
 
-export default New
+export default New;
