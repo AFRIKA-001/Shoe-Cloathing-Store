9e9709f9e3fd3523dbbc3b409f3751492d0bdd8f
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-25 12:04:11 +0300

    added lazy laoding to the whole component

--- src/components/Women.jsx
+++ src/components/Women.jsx
@@ -1,49 +1,55 @@
+import { supabase } from "../../store/supabaseClient";
+import { useState, useEffect, lazy, Suspense } from "react";
+import { Loader2, Loader } from "lucide-react";
 
-import { supabase } from "../../store/supabaseClient"
-import CustomProductHook from '../../customProductHook'
-import  {useState , useEffect}  from "react"
-import { Loader2 } from "lucide-react";
-
-
+const CustomProductHook = lazy(() => import("../../customProductHook"));
 
 function Women() {
-  
-    const [shoes,setShoes]=useState([]);
-  const [isLoading,setIsLoading]=useState(false)
+  const [shoes, setShoes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   // const [error,setError]=useState(null)
-  
-  useEffect(()=>{
-   const shoeFetching = async()=>{
-      setIsLoading(true)
-    const { data,error } = await supabase.from("women_shoes").select('*');
-    if(error){
-      console.error("there was an error during fetching")
-    }else{
-      setShoes(data)
-    } 
-    setIsLoading(false)
-    }
-    shoeFetching();
-},[])
 
-if(isLoading){
-  return <div className="text-red-600 flex justify-center  my-60 animate-spin"><Loader2/></div>
-}
+  useEffect(() => {
+    const shoeFetching = async () => {
+      setIsLoading(true);
+      const { data, error } = await supabase.from("women_shoes").select("*");
+      if (error) {
+        console.error("there was an error during fetching");
+      } else {
+        setShoes(data);
+      }
+      setIsLoading(false);
+    };
+    shoeFetching();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="text-red-600 flex justify-center  my-60 animate-spin">
+        <Loader2 />
+      </div>
+    );
+  }
 
   return (
-  <>
-  <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-    {shoes.map(product=><li key={product.id}>    
-    <CustomProductHook product={product} tableName="women_shoes"/>
-    
-    </li>)}
-     
-  </ul>
-  </>
-  )  
-      
-    
-  
+    <>
+      <Suspense
+        fallback={
+          <div className="flex justify-center my-60">
+            <Loader className="animate-spin text-red-600" />
+          </div>
+        }
+      >
+        <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+          {shoes.map((product) => (
+            <li key={product.id}>
+              <CustomProductHook product={product} tableName="women_shoes" />
+            </li>
+          ))}
+        </ul>
+      </Suspense>
+    </>
+  );
 }
 
-export default Women
+export default Women;
