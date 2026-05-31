497ecad09771ce77de60344c69619ce4a8cbf4b5
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-31 13:05:06 +0300

    added the whatsapp component to be rendered in every page

--- RootLayout.jsx
+++ RootLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./src/components/NavBar.jsx"
 import Footer from "./src/components/footer.jsx";
+import { Whatsapp } from "./whatsapp.jsx";
 
 function RootLayout() {
   return (
     <div>
      <NavBar/>
       <Outlet/>
+      <Whatsapp />
       <Footer/>
     </div>
   )
